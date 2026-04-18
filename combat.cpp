98ee0a1583858cafba4f746961276a6cf5f0a804
Gavin <u3666257@connect.hku.hk>
2026-04-18 23:35:02 +0800

    Finished Step 3 - item usage during combat

--- combat.cpp
+++ combat.cpp
@@ -27,8 +27,36 @@ void randomize_start(string &turn) {
     turn= (rand()%2) ? "player" : "enemy";
 }
 
+void removeItem(Player &player, int itemNo) {
+    for (int i=itemNo-1; i<player.inventorySize-1; i++) {
+        player.inventory[i]=player.inventory[i+1];
+    }
+    player.inventory[player.inventorySize-1]=nullptr;
+    player.inventorySize--;
+}
+
 void useItem(Player &player) {
-    cout << player.name << " tried to use an item (not implemented yet)." << endl;
+    //pops up inventory menu
+    int itemNo;
+    cin >> itemNo;
+    if (itemNo>0 && itemNo<=player.inventorySize) {
+        Item * used_item;
+        used_item=player.inventory[itemNo-1];
+        if (used_item->isWeapon) {
+            player.attack+=used_item->value;
+        }
+        else {
+            player.hp+=used_item->value;
+            if (player.hp>player.maxHp) {
+                player.hp=player.maxHp;
+            }
+        }
+        cout << player.name << " used " << used_item->name << endl;
+        removeItem(player, itemNo);
+    }
+    else {
+        cout << "item not found" << endl;
+    }
 }
 
 void encounter_combat(Player &player, Enemy &enemy) {
@@ -70,6 +98,13 @@ void encounter_combat(Player &player, Enemy &enemy) {
         if (flee) {
             break;
         }
+    }
+    if (player.hp<0) {
+        //game over
+    }
+    else {
+        //enemy defeated ui
+        //back to map ui
     }
 }
 
