baac5b7ea924b1d935343c0ce6333973f5fd438a
Gavin <u3666257@connect.hku.hk>
2026-04-18 23:09:52 +0800

    Added randomizers for starting turn and finished combat flee option

--- combat.h
+++ combat.h
@@ -10,6 +10,8 @@ int calculateDamage(int attack, int defense);
 
 void turn_change(string &turn);
 
+void randomize_start(string &turn);
+
 void useItem(Player &player);
 
 void encounter_combat(Player &player, Enemy &enemy);
