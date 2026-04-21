ed2654eebd7d2b1b43d1bb05283366e005fc6f6b
ASKaivalya <u3651968@connect.hku.hk>
2026-04-21 17:33:30 +0800

    Update enemy.cpp
    initial(2)

--- enemy.cpp
+++ enemy.cpp
@@ -11,8 +11,18 @@ enemy getEnemyForFloor(int floor) {
     } else if (floor ==3) {
         return {"Stone Beetle", 30, 8, 5, false, 1};
     } else if (floor == 4) {
-        return {"Cerberus", 150, 30, 20, true, 1};
-    } else if (floor == 4) {
+        return {"Stray Dogs", 20, 10, 6, false, 1};
+    } else if (floor == 5) {
+        return {"Venonmous Snake", 25, 13, 8, false, 1};
+    } else if (floor == 6) {
+        return {"Rabid Jackal", 30, 15, 10, false, 1};
+    } else if (floor == 7) {
+        return {"Corrupted Hound", 35, 17, 13, false, 1};
+    } else if (floor == 8) {
+        return {"Shadow Owl", 45, 20, 15, false, 1};
+    } else if (floor == 9) {
+        return {"Phantom Direwolf", 50, 25, 17, false, 1};
+    } else if (floor == 10) {
         return {"Cerberus", 150, 30, 20, true, 1};
     }
     return {"Unknown", 10, 10, 1, 1, false, 1};
